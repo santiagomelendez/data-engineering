d8591e47b5b79835d99fbc182de9b101cbdf903c
Santiago Melendez <sjaviermelendez@gmail.com>
2024-01-22 20:53:57 -0300

    Added persistence for db

---
+++ utils/persistence.py
@@ -0,0 +1,27 @@
+import os
+from sqlalchemy import create_engine, text
+
+hostname = os.environ.get('DB_HOSTNAME')
+database = os.environ.get('DB_DATABASE')
+username = os.environ.get('DB_USERNAME')
+pwd = os.environ.get('DB_PWD')
+port = int(os.environ.get('DB_PORT'))
+
+
+db_params = {
+    'host': hostname,
+    'user': username,
+    'password': pwd,
+    'database': database,
+    'port': port,
+}
+
+db_url = f'postgresql+psycopg2://{db_params["user"]}:{db_params["password"]}@{db_params["host"]}:{db_params["port"]}/{db_params["database"]}'
+
+
+engine = create_engine(db_url)
+
+
+def execute_query(query):
+    with engine.begin() as conn:
+        conn.execute(text(query))
