1e83655e454b3d6d5141d2aff8876447707bef72
Santiago Melendez <sjaviermelendez@gmail.com>
2024-02-11 01:26:22 -0300

    Extending request for fetching in a time range

--- etl/extraction.py
+++ etl/extraction.py
@@ -2,6 +2,7 @@ import requests
 import json
 import os
 import pandas as pd
+from datetime import datetime
 from json import JSONDecodeError
 from utils.pandas_helpers import save_data
 
@@ -15,10 +16,16 @@ fields = ['open_time', 'open_price', 'high_price',
 interval = '1h'
 limit = 24
 
+def get_ms_timestamp(dt: datetime):
+    if not dt:
+        return None
+    epoch = datetime(1970, 1, 1)
+    timestamp = int((dt - epoch).total_seconds() * 1000)
+    return timestamp
 
 
 # Make the request
-def fetch_klines(symbol, interval, limit):
+def fetch_klines(symbol, interval, limit=None, from_date=None, to_date=None):
     '''
     symbol	STRING	YES	
     interval	ENUM	YES	
@@ -30,8 +37,18 @@ def fetch_klines(symbol, interval, limit):
     params = {
         'symbol': symbol,
         'interval': interval,
-        'limit': limit,
     }
+    optional_params = {
+        'limit': limit,
+        'startTime': get_ms_timestamp(from_date),
+        'endTime': get_ms_timestamp(to_date)
+    }
+    
+    for k,v in optional_params.items():
+        if v:
+            params[k] = v
+    print(f'Fetching klines with params: {params}')
+
     klines_url = f'{base_url}/klines'
     headers = {
         'X-MBX-APIKEY': api_key
